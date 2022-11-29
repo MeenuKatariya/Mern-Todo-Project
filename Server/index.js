3579e90d8cd5de905493598f668a21b197698092
Meenu Katariya <102709273+MeenuKatariya@users.noreply.github.com>
2022-11-29 04:44:24 -0800

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,9 @@ app.use(cors());
 app.use(userRouter)
 app.use(TodosRouter);
 
-
+app.get('/',(req,res)=>{
+    res.send('server is started')
+    )
 app.use((req,res,next)=>{
     console.log(`${req.method} ${req.url}`);
     next();
