7639aa49d31512627179b96f4587cb4f2341ef76
Meenu Katariya <102709273+MeenuKatariya@users.noreply.github.com>
2022-12-02 21:21:08 -0800

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -14,7 +14,7 @@ app.use(TodosRouter);
 
 app.get('/',(req,res)=>{
     res.send('server is started')
-    )
+} )
 app.use((req,res,next)=>{
     console.log(`${req.method} ${req.url}`);
     next();
